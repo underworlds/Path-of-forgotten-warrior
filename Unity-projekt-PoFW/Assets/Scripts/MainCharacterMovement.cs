b9566c086368f12d8040af8dc8f935985c68e100
peskamartin <mates.13@email.cz>
2014-10-05 20:10:05 +0200

    :)

--- Unity-projekt-PoFW/Assets/Scripts/MainCharacterMovement.cs
+++ Unity-projekt-PoFW/Assets/Scripts/MainCharacterMovement.cs
@@ -1,6 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public class Hero{
+	private int health;
+	private int points;
+	private bool isFacingRight;
+	private bool isGrounded;
+
+	public Hero(){
+		this.health = 3;
+		this.points = 0;
+		this.isFacingRight = true;
+		this.isGrounded = true;
+	}
+
+	public bool IsFacingRight(){
+		return this.isFacingRight;
+	}
+
+	public void SetFacingRight(bool isFacingRight){
+		this.isFacingRight = isFacingRight;
+	}
+
+	public bool IsGrounded(){
+		return this.isGrounded;
+	}
+
+	public void SetGrounded(bool isGrounded){
+		this.isGrounded = isGrounded;
+	}
+
+
+}
+
 public class MainCharacterMovement : MonoBehaviour {
 
 	//Kuba:
@@ -8,30 +40,26 @@ public class MainCharacterMovement : MonoBehaviour {
 
 
 	//Rychlost pohybu
-	public float speed =  3.0f;
-
+	public float speed =  1.0f;
+	//Hrdina pro uchovani jeho stavu
+	private Hero hero;
 	//vyska skoku
-	private int jumpHeight = 250;
-
-	//SOUVISEJICI S KONTROLOU JSETLI JSME NA ZEMI NEBO NE .....ZATIM NEDORESENO
-	public bool isGrounded = false; //melo by byt implicitne false
-	//public RaycastHit hit = new RaycastHit();
-	//public Transform groundCheck;
-	//float groundRadius = 0.5f;
-	//public LayerMask whatIsGround;
-
-
-
-	//souvisi s metodou Flip()
-	//private bool facingRight = true;
-
-
-
+	private int jumpHeight = 280;
+	//komponenta pro detekci, zda se nachazim na zemi
+	public Transform groundCheck;
+	// radius kruhu ktery provede detekci
+	float groundRadius = 0.1f;
+	// maska urcujici co je zem
+	public LayerMask whatIsGround;
+	// promenna pro zjisteni zda je hrdina na zemi, 
+	//Pozn. nebylo mozne primo ukladat do instance hero, hazelo to error :)
+	bool isGrounded = false; //melo by byt implicitne false
 
 
 	// Use this for initialization
 	void Start () {
-	
+		//Vytvoreni hrdiny
+		this.hero = new Hero ();
 	}
 	
 	// Update is called once per frame
@@ -40,83 +68,49 @@ public class MainCharacterMovement : MonoBehaviour {
 		//left movement
 		if(Input.GetKey(KeyCode.LeftArrow) ){
 			transform.position -= Vector3.right * speed * Time.deltaTime;
-
+			if(this.hero.IsFacingRight()){
+				this.Flip();
+			}
 		}
 
 		//right movement
 		if(Input.GetKey(KeyCode.RightArrow) ){
 			transform.position += Vector3.right * speed * Time.deltaTime;
-			
+			if(!this.hero.IsFacingRight()){
+				this.Flip();
+			}
 		}
 
+		isGrounded = (Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround));
+		hero.SetGrounded (isGrounded);
+
 		//jump movement
-		if(Input.GetKeyDown(KeyCode.UpArrow)){
-			/*
-			if(isGrounded){
-				Jump();
-			}*/
+		if(Input.GetKeyDown(KeyCode.UpArrow) && isGrounded){
 
 			rigidbody2D.AddForce(new Vector3(0,jumpHeight,0));	
 
 		}
 
+
+
 	}
 
-
-
-	void Jump(){
-
-		isGrounded = false;
-		print ("JUMP METHOD");
-		rigidbody2D.AddForce(new Vector3(0,jumpHeight,0));	
-	}
 
 	void FixedUpdate(){
-// ZDE SE SNAZIM RESIT JESTLI JE NAS CHARACTER NA ZEMI ....ALE PO VSECH MOZNYCH ZKOUSKACH SE MI TO NEPODARILO...
-//		Ray ray = new Ray (this.transform.position, Vector3.down);
-		/*
-		bool hitground = Physics.Raycast (ray, out hit, 20.0f);
-		if (hitground) {
-			isGrounded = hit.distance <=10.0f;		
-		
-		}
-		Debug.DrawRay (ray.origin, ray.direction,Color.yellow);
-		Debug.Log ("Hit distance: "+hit.distance.ToString()+" Did hit: "+hitground.ToString());
 
-		*/
-
-/*
-	   isGrounded = Physics.Raycast(transform.position, -Vector3.up,hit);
-
-
-		Debug.DrawRay (transform.position, -transform.up * 2.0f, Color.green);
-
-		if (Physics.Raycast (transform.position, Vector3.right, 150.0f)) {
-
-			print ("HIT right");
-		}
-
-		print ("Grounded IS " + isGrounded);
-
-*/
 	}
-
-
-
-
+	
 	/**
 	 * Metoda z jednoho tutorialu, mela by otocit postavu tak aby koukala doleva...
 	 * Muze byt odstraneno pokud chceme aby postava jen couvala a neotacela se :) 
 	 * 
 	 **/
 
-
-	/*
 	void Flip(){ //fliping the world
-		facingRight = !facingRight;
+		this.hero.SetFacingRight(!this.hero.IsFacingRight());
 		Vector3 theScale = transform.localScale; //let me get local scale
 		theScale.x *= -1;						// flip x axis	
 		transform.localScale = theScale;		//get it back to local scale 
-	}*/
+	}
 
 }
