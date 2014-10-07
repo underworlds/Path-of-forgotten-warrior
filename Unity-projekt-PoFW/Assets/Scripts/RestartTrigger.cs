3747e8bc29348fa76fea37fb2c633ed71919625d
norddick <jakubkovarik@seznam.cz>
2014-10-07 07:59:29 +0200

    test restart triggeru

---
+++ Unity-projekt-PoFW/Assets/Scripts/RestartTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartTrigger : MonoBehaviour {
+
+	void OnTriggerEnter(Collider obj){
+		print ("ENTER THE METHOD");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
