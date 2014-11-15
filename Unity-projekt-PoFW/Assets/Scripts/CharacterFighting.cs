4644f4c07dc2c6bac626724ed45754a2a18088b4
norddick <jakubkovarik@seznam.cz>
2014-11-15 15:09:17 +0100

    ShieldCover movement

---
+++ Unity-projekt-PoFW/Assets/Scripts/CharacterFighting.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/**
+ * This class should manage all about characters fighting
+ */
+public class CharacterFighting : MonoBehaviour {
+
+	//Animator
+	private Animator anim;
+	
+	
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		if(anim == null){
+			print("There is no animator");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.S) && (Mathf.Abs(Input.GetAxis("Horizontal")) <= 0.001f )){
+			anim.SetBool("shieldCover",true);
+		}else{
+			anim.SetBool("shieldCover",false);
+		}
+	}
+}
