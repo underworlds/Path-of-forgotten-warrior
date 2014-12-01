551fe55a7a5683f9efe5b30775191863db344517
fRandOmizer <zan.richard@gmail.com>
2014-12-01 23:28:52 +0100

    Help+BestScore(bez logiky)

---
+++ Unity-projekt-PoFW/Assets/Scripts/MenuScripts/BestScoreManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BestScoreManager : MonoBehaviour {
+
+    public Texture BackgroundTexture;
+
+    public string MainMenu;
+
+    public Texture MainMenuTexture;
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0f, 0f, Screen.width, Screen.height), BackgroundTexture);
+
+        if (GUI.Button(new Rect(Screen.width * 0.135f, Screen.height * 0.87f, Screen.width / 7f, Screen.height / 7f), MainMenuTexture, ""))
+        {
+            Application.LoadLevel(MainMenu);
+        }
+    }
+}
