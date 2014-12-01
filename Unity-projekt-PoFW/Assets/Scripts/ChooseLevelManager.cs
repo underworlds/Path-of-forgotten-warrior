b271809ea8e61b8381571c45e134fd020dca021f
fRandOmizer <zan.richard@gmail.com>
2014-12-01 22:12:14 +0100

    ChooseLevel+dalsie serepeticky z menu

---
+++ Unity-projekt-PoFW/Assets/Scripts/ChooseLevelManager.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChooseLevelManager : MonoBehaviour {
+
+    public Texture BackgroundTexture;
+
+    public string MainMenu;
+
+    public string Lvl1Round1;
+    public string Lvl1Round2;
+    public string Lvl1Round3;
+    public string Lvl1Round4;
+    public string Lvl2Round1;
+    public string Lvl2Round2;
+    public string Lvl2Round3;
+    public string Lvl2Round4;
+    public string Lvl3Round1;
+    public string Lvl3Round2;
+    public string Lvl3Round3;
+    public string Lvl3Round4;
+    public string Lvl4Round1;
+    public string Lvl4Round2;
+    public string Lvl4Round3;
+    public string Lvl4Round4;
+
+    public Texture MainMenuTexture;
+
+    public Texture Lvl1Round1Texture;
+    public Texture Lvl1Round2Texture;
+    public Texture Lvl1Round3Texture;
+    public Texture Lvl1Round4Texture;
+    public Texture Lvl2Round1Texture;
+    public Texture Lvl2Round2Texture;
+    public Texture Lvl2Round3Texture;
+    public Texture Lvl2Round4Texture;
+    public Texture Lvl3Round1Texture;
+    public Texture Lvl3Round2Texture;
+    public Texture Lvl3Round3Texture;
+    public Texture Lvl3Round4Texture;
+    public Texture Lvl4Round1Texture;
+    public Texture Lvl4Round2Texture;
+    public Texture Lvl4Round3Texture;
+    public Texture Lvl4Round4Texture;
+
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0f, 0f, Screen.width, Screen.height), BackgroundTexture);
+
+        if (GUI.Button(new Rect(Screen.width * 0.02f, Screen.height * 0.12f, Screen.width / 7f, Screen.height / 7f), Lvl1Round1Texture, ""))
+        {
+            Application.LoadLevel(Lvl1Round1);
+        }
+        if (GUI.Button(new Rect(Screen.width * 0.02f + Screen.width * 0.1f, Screen.height * 0.12f, Screen.width / 7f, Screen.height / 7f), Lvl1Round2Texture, ""))
+        {
+            Application.LoadLevel(Lvl1Round2);
+        }
+        if (GUI.Button(new Rect(Screen.width * 0.02f + Screen.width * 0.1f*2, Screen.height * 0.12f, Screen.width / 7f, Screen.height / 7f), Lvl1Round3Texture, ""))
+        {
+            Application.LoadLevel(Lvl1Round3);
+        }
+        if (GUI.Button(new Rect(Screen.width * 0.02f + Screen.width * 0.1f*3, Screen.height * 0.12f, Screen.width / 7f, Screen.height / 7f), Lvl1Round4Texture, ""))
+        {
+            Application.LoadLevel(Lvl1Round4);
+        }
+
+        if (GUI.Button(new Rect(Screen.width * 0.02f, Screen.height * 0.12f + Screen.height * 0.18f, Screen.width / 7f, Screen.height / 7f), Lvl2Round1Texture, ""))
+        {
+            Application.LoadLevel(Lvl2Round1);
+        }
+        if (GUI.Button(new Rect(Screen.width * 0.02f + Screen.width * 0.1f, Screen.height * 0.12f + Screen.height * 0.18f, Screen.width / 7f, Screen.height / 7f), Lvl2Round2Texture, ""))
+        {
+            Application.LoadLevel(Lvl2Round2);
+        }
+        if (GUI.Button(new Rect(Screen.width * 0.02f + Screen.width * 0.1f * 2, Screen.height * 0.12f + Screen.height * 0.18f, Screen.width / 7f, Screen.height / 7f), Lvl2Round3Texture, ""))
+        {
+            Application.LoadLevel(Lvl2Round3);
+        }
+        if (GUI.Button(new Rect(Screen.width * 0.02f + Screen.width * 0.1f * 3, Screen.height * 0.12f + Screen.height * 0.18f, Screen.width / 7f, Screen.height / 7f), Lvl2Round4Texture, ""))
+        {
+            Application.LoadLevel(Lvl2Round4);
+        }
+
+        if (GUI.Button(new Rect(Screen.width * 0.02f, Screen.height * 0.12f + Screen.height * 0.18f*2, Screen.width / 7f, Screen.height / 7f), Lvl3Round1Texture, ""))
+        {
+            Application.LoadLevel(Lvl3Round1);
+        }
+        if (GUI.Button(new Rect(Screen.width * 0.02f + Screen.width * 0.1f, Screen.height * 0.12f + Screen.height * 0.18f * 2, Screen.width / 7f, Screen.height / 7f), Lvl3Round2Texture, ""))
+        {
+            Application.LoadLevel(Lvl3Round2);
+        }
+        if (GUI.Button(new Rect(Screen.width * 0.02f + Screen.width * 0.1f * 2, Screen.height * 0.12f + Screen.height * 0.18f * 2, Screen.width / 7f, Screen.height / 7f), Lvl3Round3Texture, ""))
+        {
+            Application.LoadLevel(Lvl3Round3);
+        }
+        if (GUI.Button(new Rect(Screen.width * 0.02f + Screen.width * 0.1f * 3, Screen.height * 0.12f + Screen.height * 0.18f * 2, Screen.width / 7f, Screen.height / 7f), Lvl3Round4Texture, ""))
+        {
+            Application.LoadLevel(Lvl3Round4);
+        }
+
+        if (GUI.Button(new Rect(Screen.width * 0.02f, Screen.height * 0.12f + Screen.height * 0.18f * 3, Screen.width / 7f, Screen.height / 7f), Lvl4Round1Texture, ""))
+        {
+            Application.LoadLevel(Lvl4Round1);
+        }
+        if (GUI.Button(new Rect(Screen.width * 0.02f + Screen.width * 0.1f, Screen.height * 0.12f + Screen.height * 0.18f * 3, Screen.width / 7f, Screen.height / 7f), Lvl4Round2Texture, ""))
+        {
+            Application.LoadLevel(Lvl4Round2);
+        }
+        if (GUI.Button(new Rect(Screen.width * 0.02f + Screen.width * 0.1f * 2, Screen.height * 0.12f + Screen.height * 0.18f * 3, Screen.width / 7f, Screen.height / 7f), Lvl4Round3Texture, ""))
+        {
+            Application.LoadLevel(Lvl4Round3);
+        }
+        if (GUI.Button(new Rect(Screen.width * 0.02f + Screen.width * 0.1f * 3, Screen.height * 0.12f + Screen.height * 0.18f * 3, Screen.width / 7f, Screen.height / 7f), Lvl4Round4Texture, ""))
+        {
+            Application.LoadLevel(Lvl4Round4);
+        }
+
+        if (GUI.Button(new Rect(Screen.width * 0.138f, Screen.height * 0.12f + Screen.height * 0.18f * 4, Screen.width / 7f, Screen.height / 7f), MainMenuTexture, ""))
+        {
+            Application.LoadLevel(MainMenu);
+        }
+
+    }
+}
--- Unity-projekt-PoFW/Assets/Scripts/MenuCameraBehaviour.cs
+++ Unity-projekt-PoFW/Assets/Scripts/MenuCameraBehaviour.cs
@@ -6,7 +6,12 @@ public class MenuCameraBehaviour : MonoBehaviour {
     public Texture BackgroundTexture;
     public Texture NewGame;
 
-    public string Level1Round1;
+    public string Level1Round1Scene;
+    public string ChooseLevelScene;
+    public string BestScoreScene;
+    public string CreditsScene;
+    public string HelpScene;
+    
 
     public Texture Continue;
     public Texture ChooseLevel;
@@ -21,19 +26,34 @@ public class MenuCameraBehaviour : MonoBehaviour {
 
         if (GUI.Button(new Rect(Screen.width * 0.1f, Screen.height * 0.12f, Screen.width / 5f, Screen.height / 10f), NewGame, ""))
         {
-			GameManager gm = GameObject.FindObjectOfType<GameManager>();
+			/*GameManager gm = GameObject.FindObjectOfType<GameManager>();
 			gm.inMainMenu = false;
 			gm.TotalReset();
-			gm.loadComponents();
+			gm.loadComponents();*/
 
-            Application.LoadLevel(Level1Round1);
+            Application.LoadLevel(Level1Round1Scene);
 
         }
-        GUI.Button(new Rect(Screen.width * 0.1f, Screen.height * 0.12f + Screen.height * 0.11f, Screen.width / 5f, Screen.height / 10f), Continue, "");
-        GUI.Button(new Rect(Screen.width * 0.1f, Screen.height * 0.12f + Screen.height * 0.11f * 2f, Screen.width / 5f, Screen.height / 10f), ChooseLevel, "");
-        GUI.Button(new Rect(Screen.width * 0.1f, Screen.height * 0.12f + Screen.height * 0.11f * 3f, Screen.width / 5f, Screen.height / 10f), BestScore, "");
-        GUI.Button(new Rect(Screen.width * 0.1f, Screen.height * 0.12f + Screen.height * 0.11f * 4f, Screen.width / 5f, Screen.height / 10f), Credits, "");
-        GUI.Button(new Rect(Screen.width * 0.1f, Screen.height * 0.12f + Screen.height * 0.11f * 5f, Screen.width / 5f, Screen.height / 10f), Help, "");
+        if (GUI.Button(new Rect(Screen.width * 0.1f, Screen.height * 0.12f + Screen.height * 0.11f, Screen.width / 5f, Screen.height / 10f), Continue, ""))
+        {
+
+        }
+        if(GUI.Button(new Rect(Screen.width * 0.1f, Screen.height * 0.12f + Screen.height * 0.11f * 2f, Screen.width / 5f, Screen.height / 10f), ChooseLevel, ""))
+        {
+            Application.LoadLevel(ChooseLevelScene);
+        }
+        if(GUI.Button(new Rect(Screen.width * 0.1f, Screen.height * 0.12f + Screen.height * 0.11f * 3f, Screen.width / 5f, Screen.height / 10f), BestScore, ""))
+        {
+            Application.LoadLevel(BestScoreScene);
+        }
+        if(GUI.Button(new Rect(Screen.width * 0.1f, Screen.height * 0.12f + Screen.height * 0.11f * 4f, Screen.width / 5f, Screen.height / 10f), Credits, ""))
+        {
+            Application.LoadLevel(CreditsScene);
+        }
+        if (GUI.Button(new Rect(Screen.width * 0.1f, Screen.height * 0.12f + Screen.height * 0.11f * 5f, Screen.width / 5f, Screen.height / 10f), Help, ""))
+        {
+            Application.LoadLevel(HelpScene);
+        }
         if (GUI.Button(new Rect(Screen.width * 0.1f, Screen.height * 0.12f + Screen.height * 0.11f * 6f, Screen.width / 5f, Screen.height / 10f), Exit, ""))
         {
             Application.Quit();
